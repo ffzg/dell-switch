9396b238ef41c8896115bed4675066d5d2510c4d
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-04-11 16:47:06 +0200

    rename script to snmp-mac-port and create same /dev/shm files
    moved filtering of snmpwalk output into script itself

--- snmp-grep.sh
+++ snmp-grep.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 patt=$*
 test -z "$patt" && patt='.'
-grep -r -i $patt /dev/shm/sw.mac.port/ | sed -e 's/^.*snmp.//' -e 's/:[^:]*: / /' -e 's/\].*: / /'
+grep -r -i $patt /dev/shm/sw.mac.port/
