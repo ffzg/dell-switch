a3e9e6f65fd50e3c6cbafef639c41ded109afb70
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-04-24 19:13:50 +0200

    create dump files in /dev/shm/snmp-mac

--- snmp-arp.sh
+++ snmp-arp.sh
@@ -1,2 +1,9 @@
+#!/bin/sh -xe
+
 . ./snmp.conf 
-snmpwalk -v2c -Cc -c $COMMUNITY $1 1.3.6.1.2.1.4.22 # (ipNetToMediaTable)
+
+log=/dev/shm/snmp-mac
+test -d $log || mkdir $log
+
+# (ipNetToMediaTable)
+snmpbulkwalk -v2c -Cc -c $COMMUNITY $1 1.3.6.1.2.1.4.22 | tee $log/$1
