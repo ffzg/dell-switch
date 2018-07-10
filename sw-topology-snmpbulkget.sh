cdc08ca0e7eea45a4788a2ec37c61eb07a6bf577
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-07-10 18:47:19 +0200

    faster snmpbulkget for topology

---
+++ sw-topology-snmpbulkget.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+# Usage: $0 [sw [oid]]
+
+dir=/dev/shm/snmp-topology/
+test ! -d $dir && mkdir $dir
+
+. ./snmp.conf
+
+ext=""
+if [ ! -z "$2" ] ; then
+	ext=".$2/"
+	test ! -d "$dir/$ext" && mkdir "$dir/$ext"
+fi
+
+( test ! -z "$1" && echo $1 || ./sw-names ) | xargs -i echo \
+"test ! -e $dir/{} && " \
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} IF-MIB::ifPhysAddress        | tee -a $dir/{}     && "\
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} Q-BRIDGE-MIB::dot1qTpFdbPort | tee -a $dir/$ext{} || "\
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} BRIDGE-MIB::dot1dTpFdbPort   | tee -a $dir/{} " | sh -x
+
+
