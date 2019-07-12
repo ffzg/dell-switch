9a1d02bd01ba613a3c75609566c43b71870d92aa
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-07-12 10:07:06 +0200

    create svg using graphviz

--- sw-topology-snmpbulkget.sh
+++ sw-topology-snmpbulkget.sh
@@ -10,16 +10,15 @@ fi
 
 . ./snmp.conf
 
-ext=""
-if [ ! -z "$2" ] ; then
-	ext=".$2/"
-	test ! -d "$dir/$ext" && mkdir "$dir/$ext"
-fi
-
 ( test ! -z "$1" && echo $1 || ./sw-names ) | xargs -i echo \
-"test ! -e $dir/{} && " \
-"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} IF-MIB::ifPhysAddress        | tee -a $dir/{}     && "\
-"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} Q-BRIDGE-MIB::dot1qTpFdbPort | tee -a $dir/$ext{} || "\
-"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} BRIDGE-MIB::dot1dTpFdbPort   | tee -a $dir/{} " | sh -x
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} Q-BRIDGE-MIB::dot1qTpFdbPort > $dir/{} ; "\
+'test `cat '$dir'/{} | wc -l` -le 1 && ' \
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} BRIDGE-MIB::dot1dTpFdbPort   >> $dir/{} ; " \
+"snmpbulkwalk -OX -v2c -Cc -c $COMMUNITY {} IF-MIB::ifPhysAddress        >> $dir/{} "\
+| tee /dev/shm/snmp-topology-snmpbulkwalk.sh | sh -x
 
 
+# ./wap-mac-list.sh
+./snmp-topology.pl
+
+dot -Tsvg -o /var/www/snmp-topology-2.svg /tmp/snmp-topology.dot
