a0fd4557022145377e9f21023a85cee83bb2df10
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-01-27 10:02:15 +0100

    grep mac anywhere

--- bro-conn-ip-vlan-mac.sh
+++ bro-conn-ip-vlan-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-ssh enesej "cat /var/log/bro/current/conn.log | bro-cut -d id.orig_h vlan orig_l2_addr -F' ' | grep '^$1 ' | head -1"
+ssh enesej "cat /var/log/bro/current/conn.log | bro-cut -d id.orig_h vlan orig_l2_addr -F' ' | grep ' $1 ' | head -1"
 
