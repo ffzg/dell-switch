1b821c884b34224be56c2fa38531f0c0d0404e75
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-04-04 14:52:24 +0200

    parse MikroTik /ip neighbor print detail terse

--- neighbors.pl
+++ neighbors.pl
@@ -8,6 +8,8 @@ use autodie;
 
 use Data::Dump qw(dump);
 
+my $debug = $ENV{DEBUG} || 0;
+
 my $mac2ip;
 my $mac2name;
 
@@ -19,11 +21,13 @@ while(<$f>) {
 	$mac2name->{$mac} = $name;
 }
 
-warn "# mac2name = ",dump($mac2name);
+warn "# mac2name = ",dump($mac2name) if $debug;
+
+# parse Dell switch lldp neighbors output
 
 foreach my $file ( glob('log/*lldp*') ) {
 	my ( undef, $name, undef ) = split(/_/, $file);
-	print "# $name $file\n";
+	print "# $name $file\n" if $debug;
 
 	my $line_regex;
 	my @ports;
@@ -46,7 +50,7 @@ foreach my $file ( glob('log/*lldp*') ) {
 				$line_regex =~ s{\(\.+\)$}{(.+)}g;
 			}
 
-			print "## line_regex = $line_regex\n";
+			print "## line_regex = $line_regex\n" if $debug;
 			next;
 		}
 
@@ -77,7 +81,7 @@ foreach my $file ( glob('log/*lldp*') ) {
 				die "don't know how to parse $file";
 			}
 
-			print "# [$_] ",join('|',@v),$/;
+			print "# [$_] ",join('|',@v),$/ if $debug;
 
 			@v = map { s/^\s+//; s/\s+$//; $_ } @v;
 
@@ -107,12 +111,53 @@ foreach my $file ( glob('log/*lldp*') ) {
 				push @ports, [ @v ];
 			}
 		} else {
-			print "# $_<--\n";
+			print "# $_<--\n" if $debug;
 		}
 	}
 
 	foreach my $p ( @ports ) {
 		next if ( $p->[1] eq $p->[2] && $p->[3] eq '' ); # FIXME hosts?
 		print "$name ", join(' | ', @$p ), "\n";
+	}
+}
+
+# prase MikroTik /ip neighbor print detail terse
+
+foreach my $file ( glob('../mikrotik-switch/out/*neighbor*') ) {
+	my $name = $1 if $file =~ m{out/([\w\-]+)\.ip neighbor};
+	print "## [$name] file $file\n";
+	open(my $f, '<', $file);
+	while(<$f>) {
+		chomp;
+		next if m/^\s*$/;
+		s{^\s*\d+\s+}{}; # remote ordinal number
+		print "# $_\n";
+		my $l;
+		foreach my $kv ( split(/ /, $_) ) {
+			my ($k,$v) = split(/=/, $kv);
+			$l->{$k} = $v;
+		}
+		warn "## l=",dump($l),$/;
+		# Port       Device ID          Port ID          System Name     Capabilities 
+		my @v = (
+			$l->{interface},
+			$l->{'mac-address'},
+			$l->{'interface-name'},
+			$l->{'identity'},
+			#$l->{caps},
+			join(' ', $l->{address}, $l->{platform}, $l->{board}, $l->{version} ),
+		);
+
+			# FIXME: duplicated from above
+			if ( exists $mac2name->{$v[1]} ) {
+				my $mac_name = $mac2name->{$v[1]};
+				if ( $v[3] eq '' ) {
+					$v[3] = $mac_name;
+				} else {
+					warn "ERROR: name different $v[3] != $mac_name" if $v[3] ne $mac_name;
+				}
+			}
+
+		print "$name ", join(' | ', @v), $/;
 	}
 }
