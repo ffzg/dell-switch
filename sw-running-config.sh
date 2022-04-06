4c10035b76d3683190c78cc6639e61bab1fe1bb3
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-04-06 09:56:12 +0200

    dump running or startup config (for comparison)

--- sw-running-config.sh
+++ sw-running-config.sh
@@ -2,7 +2,9 @@
 
 sw=$1
 
-NO_LOG=1 ./dell-switch.pl $sw "copy running-config tftp://10.20.0.216/$sw"
+config=$( basename $0 | sed -e 's/^sw-//' -e 's/.sh$//' )
+
+NO_LOG=1 ./dell-switch.pl $sw "copy $config tftp://10.20.0.216/$sw"
 cd running-config
 git add $sw
-git commit -m $sw
+git commit -m "$sw $config"
---
+++ sw-startup-config.sh
@@ -0,0 +1,1 @@
+sw-running-config.sh
