922dd20d37c2fa0abc75dd2563aeaad89ec0fc0c
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-07-09 20:09:46 +0200

    save /dev/shm/wap-name-mac to git

--- wap-name-mac.sh
+++ wap-name-mac.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+test -e /dev/shm/.git || ln -sv $( pwd )/shm/.git /dev/shm/.git
+
 # seed arp
 ./wap-names | xargs fping -q
 # extract mac
 /usr/sbin/arp -a | grep wap | awk '{ print $1" "$4 }' | sed 's/\.infosl//' | tee /dev/shm/wap-name-mac
+
+git -C /dev/shm commit -m $( date +%Y-%m-%d ) -a
