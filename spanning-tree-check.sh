ab75e666505d966b8be495170fdba86f7eee5a9f
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-08-13 13:15:44 +0200

    show full data about active roots

--- spanning-tree-check.sh
+++ spanning-tree-check.sh
@@ -3,7 +3,7 @@
 # check for files older than day
 
 find log -name '*active*' -ctime +1  | cut -d_ -f2 | while read sw ; do
-	#./dell-switch.pl $sw 'show spanning-tree active'
+	./dell-switch.pl $sw 'show spanning-tree active'
 	echo -n '.'
 done
 
@@ -11,7 +11,7 @@ done
 #git -C out grep STP | grep -v RSTP | grep 'spanning-tree active' | sed -e 's/_/ /g'  | awk '{ print $2" .*"$6 }' > /tmp/st.1.patt
 
 echo "# active roots"
-git -C out grep root '*active*'
+git -C out grep -B 2 root '*active*'
 
 echo "# root bridge"
 (
@@ -19,6 +19,6 @@ git -C ../mikrotik-switch/out grep root-bridge-id | cut -d. -f 1,3 | sed 's/\./ 
 git -C out grep -A 4 'Root ID' '*active*' | grep Address | sed 's/_/ /g' | awk '{ print $2 " " $7 }'
 ) | sort -k 2 | column -t
 
-echo "# STP only (RSTP ignored)"
+echo "# STP only (RSTP ignored) -- should be empty if OK"
 git -C out grep STP | grep -v RSTP | grep 'spanning-tree active' | tee /tmp/st.2.full | sed -e 's/_/ /g'  | awk '{ print $6"\t"$2"[ \t$]" }' > /tmp/st.2.patt
 grep -f /tmp/st.2.patt /dev/shm/neighbors.tab | column -t
