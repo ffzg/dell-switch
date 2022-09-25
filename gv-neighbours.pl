5a83baf94e0dac3631aee58f1d9e0af409ce24fa
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-09-25 19:41:59 +0200

    use first destination port

--- gv-neighbours.pl
+++ gv-neighbours.pl
@@ -5,7 +5,10 @@ use autodie;
 use Storable;
 use Data::Dump qw(dump);
 
+$|=1;
+
 my $gv = Storable::retrieve( '/tmp/gv.storable' );
+delete $gv->{'sw-core'}->{$_} foreach 7 .. 10; # remove sw-b101 lacp
 warn "# gv = ",dump( $gv );
 
 my @sw = qw( sw-core );
@@ -28,7 +31,12 @@ foreach my $sw ( @sw ) {
 warn "XX $sw";
 	foreach my $port ( sort { $a <=> $b } keys %$s ) {
 		my $to_sw   = ( keys %{ $s->{$port}           } )[0];
-		my $to_port = ( keys %{ $s->{$port}->{$to_sw} } )[0];
+		my @to_ports=   keys %{ $s->{$port}->{$to_sw} } ;
+		my $to_port = $to_ports[0];
+
+		if ( scalar @to_ports > 1 ) {
+			warn "ERROR $sw $port -- $to_sw more than one destination port ",dump( @to_ports );
+		}
 
 		next if $to_port eq 'no_port';
 
@@ -37,6 +45,8 @@ warn "XX $sw";
 		( $port, $to_port ) = @{ $s->{$port}->{$to_sw}->{$to_port} }; # replace port number with name
 
 		print "$prefix $sw $port $to_sw $to_port\n";
+
+		next if $done_sw->{$to_sw};
 
 		if ( $to_sw =~ m/^sw-/ ) {
 			push @prefix, "$prefix $sw $port $to_sw $to_port";
